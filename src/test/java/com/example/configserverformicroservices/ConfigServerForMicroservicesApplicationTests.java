78d3bc3d3dc009eb7572ce199eddd28c600877fb
Kuldeep Kumar <Kuldeep.Kumar@oodlesTechnologies.com>
2019-12-16 12:25:57 +0530

    Config Server With Application properties on github

---
+++ src/test/java/com/example/configserverformicroservices/ConfigServerForMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverformicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerForMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
